9446fe6b6ae38e9c76b55fde9d3b0fd2278b51ea
Filip Timko <filip.timko@protonmail.com>
2025-06-29 22:19:58 +0200

    set font to fira code

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,14 +4,11 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
-config.font_size = 11
+config.font = wezterm.font 'Fira Code'
+config.font_size = 12
 config.color_scheme = 'Catppuccin Mocha'
 
 config.window_background_opacity = 0.95
